0f8d1aef0036121c448b1719d53d0ee978644ed0
Victor Fernando de Oliveira Sampaio <victorsampaio@users.noreply.github.com>
2020-03-30 18:09:57 -0300

    Flutter Project
    Flutter Project

---
+++ imc_app/lib/main.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+  String _infoText = "Informe seus dados";
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _resetFild() {
+    weightController.text = "";
+    heightController.text = "";
+    setState(() {
+      _infoText = "Informe seus dados";
+      _formKey = GlobalKey<FormState>();
+    });
+  }
+
+  void _infoApp(){
+    Fluttertoast.showToast(
+        msg: "Desenvolvido por Victor Sampaio",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.teal,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      print(imc);
+      if (imc < 18.6) {
+        _infoText = "Abeixo do Peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso Ideal (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente acima do peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 24.9 && imc < 34.9) {
+        _infoText = "Obesidade Grau I: (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade Grau II: (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 40) {
+        _infoText = "Obesidade Grau III: (${imc.toStringAsPrecision(4)})";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Calculadora de IMC"),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: _resetFild,
+            ),
+            IconButton(
+              icon: Icon(Icons.info_outline),
+              onPressed: _infoApp,
+            )
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+            padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Icon(
+                    Icons.person_outline,
+                    size: 120.0,
+                    color: Colors.green,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Peso (Kg)",
+                      labelStyle: TextStyle(color: Colors.green),
+                    ),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25.0),
+                    controller: weightController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Inseira sua Peso";
+                      }
+                    },
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Altura (cm)",
+                      labelStyle: TextStyle(color: Colors.green),
+                    ),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25.0),
+                    controller: heightController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Inseira sua Altura";
+                      }
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                    child: Container(
+                      height: 50.0,
+                      child: RaisedButton(
+                        onPressed: () {
+                          if (_formKey.currentState.validate()) {
+                            _calculate();
+                          }
+                        },
+                        child: Text(
+                          "Calcular",
+                          style: TextStyle(color: Colors.white, fontSize: 25.0),
+                        ),
+                        color: Colors.green,
+                      ),
+                    ),
+                  ),
+                  Text(_infoText,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.green, fontSize: 25.0))
+                ],
+              ),
+            )));
+  }
+}
